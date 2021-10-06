7f52b276815cb9f46796d41808dfc687c596d0cc
FahimHassanMollah <fahimhassan0197@gmail.com>
2021-10-06 23:46:43 +0600

    routing added

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+  <div class="container">
+    <router-link :to="{ name:'Home' }" class="navbar-brand">Navbar</router-link>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <router-link class="nav-link" :to="{name:'Create' }">Create</router-link>
+        </li>
+    </ul>
+    
+    
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name:"Nav"
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 const routes = [
+  
   {
     path: '/',
     name: 'Home',
-    component: Home
+
+    component: () => import(/* webpackChunkName: "Home" */ '../views/Home.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/create',
+    name: 'Create',
+    
+    component: () => import(/* webpackChunkName: "Create" */ '../views/Create.vue')
+  },
+  {
+    path: '/posts/:id',
+    name: 'Details',
+    
+    component: () => import(/* webpackChunkName: "Details" */ '../views/Details.vue')
+  },
+  {
+    path: '/tags/:tag',
+    name: 'Tag',
+    
+    component: () => import(/* webpackChunkName: "Tag" */ '../views/Tag.vue')
   }
 ]
 
